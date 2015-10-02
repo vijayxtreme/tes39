7568d986787a0e6a8394f4eaa4774c72b43fa06e
Equate <Equate@Admins-Mac-mini.local>
2015-10-02 16:30:41 -0700

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name bvl39App
+ * @description
+ * # bvl39App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('bvl39App', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bvl39App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bvl39App
+ */
+angular.module('bvl39App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
