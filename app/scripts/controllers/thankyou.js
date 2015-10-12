7bae52336ca3bf2346d632ec85c65d8592881f1e
Equate <Equate@Admins-Mac-mini.local>
2015-10-12 13:01:21 -0700

    Step 3 CSS Update

--- app/scripts/controllers/thankyou.js
+++ app/scripts/controllers/thankyou.js
@@ -10,7 +10,10 @@
 angular.module('bvl39App')
   .controller('thankYouCtrl', ['$scope', '$location', '$rootScope', function ($scope, $location, $rootScope) {
  	
-  	
+  	$scope.first_name =  "Vijay";
+  	$scope.last_name =  "Menon";
+  	$scope.price1 = "999";
+  	$scope.price2 = "9999"
 
 
  }]);
